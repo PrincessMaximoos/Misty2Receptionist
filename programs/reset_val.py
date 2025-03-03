94ee8bdeff336efad10e97f993847f80b8b6907c
PrincessMaximoos <m016770n@student.staffs.ac.uk>
2025-03-03 02:13:54 +0000

    Delete programs directory

--- programs/reset_val.py
+++
@@ -1,7 +0,0 @@
-ids : list[dict] = [{"Name" : "Name", "Company" : "Company", "IDNum" : "IDNum", "Amount" : "Amount"}]
-visitor_count = 0
-
-import json
-
-with open("output/visitor_info.json", "w") as f:
-    json.dump({"result" : ids, "num" : visitor_count}, f)
